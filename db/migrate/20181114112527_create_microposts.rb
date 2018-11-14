1259a3c3048580c3d3da3c43ba0240779cd0e881
celtic93 <egorka80@list.ru>
2018-11-14 16:38:19 +0300

    Added reference

--- db/migrate/20181114112527_create_microposts.rb
+++ db/migrate/20181114112527_create_microposts.rb
@@ -2,7 +2,7 @@ class CreateMicroposts < ActiveRecord::Migration[5.2]
   def change
     create_table :microposts do |t|
       t.text :content
-      t.integer :user_id
+      t.references :user
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -17,9 +17,10 @@ ActiveRecord::Schema.define(version: 2018_11_14_112527) do
 
   create_table "microposts", force: :cascade do |t|
     t.text "content"
-    t.integer "user_id"
+    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_microposts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
