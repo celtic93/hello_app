a3f257196f6fb28aa94179f01af71d72563df19d
celtic93 <egorka80@list.ru>
2018-11-14 10:45:45 +0300

    Changed greeting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render html: 'Hello, world!'.html_safe    
+    render html: 'Hello, Denmark!'.html_safe    
   end
 
   def goodbye
